5d1526ecd191ef8de60976330546392c884f4803
theunsungdesigner <theunsungdesigner@gmail.com>
2019-08-09 16:07:24 -0400

    changes

--- 20190808/homework 2/04-Loan-Calculator/starter-code/LoanCalculator/src/main/java/com/company/LoanCalculator.java
+++ 20190808/homework 2/04-Loan-Calculator/starter-code/LoanCalculator/src/main/java/com/company/LoanCalculator.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
 package com.company;
+//        i = IRR
+//        L = Loan amount
+//        F = Points and all other lender fees
+//        P = Monthly payment
+//        n = Month when the balance is paid in full
+//        Bn = Balance in month n
 
+//P = L[c(1 + c)n]/[(1 + c)n - 1]
 public class LoanCalculator {
     public static void main(String[] args) {
+
+        System.out.println( "Whats your monthly payment?");
+        int userPayment = Integer.parseInt(scan.nextLine());
+
+        System.out.println(userPayment);
+        System.out.println("Whats your Loan amount?");
+
+
+
 
     }
 }
