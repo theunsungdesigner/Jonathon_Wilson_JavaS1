e3c5ced0144e946d49e8b366c264b43ca6fe1eac
theunsungdesigner <theunsungdesigner@gmail.com>
2019-08-11 14:37:26 -0400

    changes

---
+++ 20190809/homework/katas-5/arrayExample/src/main/java/arrayExample.java
@@ -0,0 +1,23 @@
+
+
+
+import java.util.Arrays;
+
+    public class arrayExample {
+
+        public static void main(String[] args) {
+
+            int[] arr1 = new int[4];
+            arr1[0] = 10;
+            arr1[1] = 20;
+            arr1[2] = 30;
+            arr1[3] = 40;
+
+            System.out.println("Array 1: " + Arrays.toString(arr1));
+            System.out.println("Array 1 position 0: " + arr1[0]);
+            System.out.println("Array 1 position 1: " + arr1[1]);
+            System.out.println("Array 1 position 2: " + arr1[2]);
+            System.out.println("Array 1 position 3: " + arr1[3]);
+
+        }
+    }
