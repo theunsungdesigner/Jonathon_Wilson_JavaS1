63f5bdb84f2677f83d1dd7d8acd95dced32bbd40
theunsungdesigner <theunsungdesigner@gmail.com>
2019-08-12 16:46:49 -0400

    changes for homework

--- 20190810/katas/starter-code/U1M1L6Katas/src/main/java/app2.java
+++ 20190810/katas/starter-code/U1M1L6Katas/src/main/java/app2.java
@@ -2,13 +2,19 @@ import java.util.Arrays;
 public class app2 {
 
 
+// Question 1 method of adding
+
     public static void main(String[] args) {
+        totalOdds();
+        totalSum();
+    }
+
+    public static void totalSum() {
+
 
         int[] nums1 = {0, 1, 2, 3, 4, 5, 6, 7};
-
-//        int sum1 = nums1.length;
         int sum1 = 0;
-
+//        total();
 
         for (int i = 0; i < nums1.length; i++) {
 
@@ -17,8 +23,23 @@ public class app2 {
             System.out.println(sum1);
 
         }
-        
+
+    }
+
+    public static void totalOdds() {
+        int[] nums2 = {0, 1, 2, 3, 4, 5, 6, 7};
+        int sum2 = 0;
+        for (int i = 0; i < nums2.length; i++) {
+
+//            sum2 += nums2[i];
+
+            System.out.println(sum2);
+
+            sum2 = nums2[2] + nums2[4] + nums2[6] + nums2[7];
+        }
+        ;
 
     }
 }
+
 
